ee3308e664efa3a75e5b9d507e19d824463ac8d0
Raveen Dharmasiri <work.raveendharmasiri@gmail.com>
2022-06-11 12:41:32 +0530

    Finished implementing the header

---
+++ src/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import ChatIcon from '@material-ui/icons/Chat';
+
+import HeaderOption from "./HeaderOption";
+
+function Header() {
+    return(
+        <div className="header">
+            <div className="header__left">
+                <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Messaging" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar="https://cdn2.vectorstock.com/i/1000x1000/23/81/default-avatar-profile-icon-vector-18942381.jpg" title="me" />
+            </div>
+        </div>
+    );
+}
+
+export default Header;
