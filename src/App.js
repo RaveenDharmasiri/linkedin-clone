493eadef83102ed25b8a748d5a145f1fe214874a
Raveen Dharmasiri <work.raveendharmasiri@gmail.com>
2022-06-12 12:13:13 +0530

    Styled home screen sections

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Login from './components/Login';
-import Home from './components/Home';
+import Home from './components/Home/Home';
 
 function App() {
   return (
